b6fbd8a112e81b521d5a693349a5239a39282abd
Vlad <lutzoww51@gmail.com>
2025-02-04 11:42:18 +0500

    Update main.cpp

--- Integer/main.cpp
+++ Integer/main.cpp
@@ -7,15 +7,21 @@ int main() {
     Integer b = 3;
     Integer c = -2;
     Integer d = -4;
+    Integer tt = -1;
 
-    std::cout << a - b << "\n";
-    std::cout << c - d << "\n";
-    std::cout << c - a << "\n";
-    std::cout << c - c << "\n";
-    std::cout << d - a << "\n";
-    std::cout << b - d << "\n";
-
-
-    Rational al(b, a);
-    std::cout << al.properFraction();
+    std::cout << 5 % 3 << "\n";
+    std::cout << -2 % -4 << "\n";
+    std::cout << -2 % 5 << "\n";
+    std::cout << -2 % -2 << "\n";
+    std::cout << -4 % 5 << "\n";
+    std::cout << 3 % -4 << "\n";
+    std::cout << "-----------------\n";
+    std::cout << a % b << "\n";
+    std::cout << c % d << "\n";
+    std::cout << c % a << "\n";
+    std::cout << c % c << "\n";
+    std::cout << d % a << "\n";
+    std::cout << b % d << "\n";
+    a += b;
+    std::cout << "\n\n\n" << a;
 }
